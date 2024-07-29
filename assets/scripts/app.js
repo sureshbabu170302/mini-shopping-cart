5940e24f7dadedcad30cb412d986a315a9cf034a
sureshbabu170302 <sureshbabu.kanini@gmail.com>
2024-07-29 09:51:28 +0530

    Updated app.js code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
 class Product {
-  title = "DEFAULT";
-  imageUrl;
-  description;
-  price;
+  // title = "DEFAULT";
+  // imageUrl;
+  // description;
+  // price;
 
   constructor(title, imageUrl, desc, price) {
     this.title = title;
@@ -12,8 +12,31 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+            <div>
+                <img src = "${this.product.imageUrl}" alt = "${this.product.title}">
+                <div class = "product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>$${this.product.price}</h3> 
+                    <p>${this.product.description}</p>
+                    <button>Add to cart</button>
+                </div>
+            </div>
+        `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A Pillow",
       "https://media.istockphoto.com/id/1018424252/photo/white-pillow-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=isDoOy8kYXAshwL9MRjWrWA7OJ8U0L9ub_dFAkjxYLg=",
@@ -32,30 +55,21 @@ const productList = {
       "A antique flower vase",
       100.0
     ),
-  ],
+  ];
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-            <div>
-                <img src = "${prod.imageUrl}" alt = "${prod.title}">
-                <div class = "product-item__content">
-                    <h2>${prod.title}</h2>
-                    <h3>$${prod.price}</h3> 
-                    <p>${prod.description}</p>
-                    <button>Add to cart</button>
-                </div>
-            </div>
-        `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
-console.log(productList);
